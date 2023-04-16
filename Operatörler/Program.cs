84773894aad1edd169c7f991a118b65c81591e5b
Funda Yıldırımkaraman <108402804+fundayldrmkaraman@users.noreply.github.com>
2023-04-16 17:25:19 +0300

    Add files via upload

---
+++ Operatörler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Operatörler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            bool isSucces = true;
+            bool isCompleted = false;
+
+            if (isSucces && isCompleted)
+                Console.WriteLine("Perfesct!");
+
+            if (isSucces || isCompleted)
+                Console.WriteLine("Great!");
+
+            if (isSucces && isCompleted)
+                Console.WriteLine("Fine!");
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+        }
+    }
+}
